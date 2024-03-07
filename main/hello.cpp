a42a049f004fdcb4fbfedf96a1e36ee4f2e6bc0f
DanishAhmed33 <da372725@gmail.com>
2024-03-07 15:36:33 +0530

    Describe this commit6

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run thanks\n";
+  cout << "I have successfully built and run thanks for everything\n";
   return 0;
 }
